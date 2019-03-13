99f6ecb743aeaef3e4d51bde7f9ce4e6d3da8de8
huaxi.xie <huaxi.xie@vipshop.com>
2019-03-13 15:32:28 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -78,7 +78,6 @@
         var query = input.value;
         var xhr = new XMLHttpRequest();
         var url = '/book?q=' + query + '&fields=id,title,image,author,publisher,price';
-        var cacheData;
         if (query === '') {
             tip('请输入关键词');
             return;
@@ -86,24 +85,30 @@
         document.querySelector('#js-list').innerHTML = '';
         document.querySelector('#js-thanks').style = 'display: none';
         loading(true);
-        var remotePromise = getApiDataRemote(url);
-        getApiDataFromCache(url).then(function (data) {
-            if (data) {
+        xhr.timeout = 60000;
+        xhr.onreadystatechange = function () {
+            var response = {};
+            if (xhr.readyState === 4 && xhr.status === 200) {
                 loading(false);
-                input.blur();            
-                fillList(data.books);
-                document.querySelector('#js-thanks').style = 'display: block';
+                try {
+                    response = JSON.parse(xhr.responseText);
+                }
+                catch (e) {
+                    response = xhr.responseText;
+                }
+                tip();
+                if (response.books.length === 0) {
+                    tip('无结果');
+                }
+                else {
+                    input.blur();
+                    fillList(response.books);
+                    document.querySelector('#js-thanks').style = 'display: block';
+                }
             }
-            cacheData = data || {};
-            return remotePromise;
-        }).then(function (data) {
-            if (JSON.stringify(data) !== JSON.stringify(cacheData)) {
-                loading(false);                
-                input.blur();
-                fillList(data.books);
-                document.querySelector('#js-thanks').style = 'display: block';
-            }
-        });
+        };
+        xhr.open('GET', url, true);
+        xhr.send(null);
     }
 
     /**
@@ -122,134 +127,12 @@
         }
     });
 
-    /**
-     * 获取该请求的缓存数据
-     * @param {string} url 请求的url
-     * @return {Promise}
-     */
-    function getApiDataFromCache(url) {
-        if ('caches' in window) {
-            return caches.match(url).then(function (cache) {
-                if (!cache) {
-                    return;
-                }
-                return cache.json();
-            });
-        }
-        else {
-            return Promise.resolve();
-        }
-    }
 
-    function getApiDataRemote(url) {
-        return new Promise(function (resolve, reject) {
-            var xhr = new XMLHttpRequest();
-            xhr.timeout = 60000;
-            xhr.onreadystatechange = function () {
-                var response = {};
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    try {
-                        response = JSON.parse(xhr.responseText);
-                    }
-                    catch (e) {
-                        response = xhr.responseText;
-                    }
-                    resolve(response);
-                }
-                else if (xhr.readyState === 4) {
-                    resolve();
-                }
-            };
-            xhr.onabort = reject;
-            xhr.onerror = reject;
-            xhr.ontimeout = reject;
-            xhr.open('GET', url, true);
-            xhr.send(null);
-        });
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('./sw.js').then(function() {
+        console.log('service worker 注册成功');
+      }).catch(function(err) {
+        console.log('error => ' + err)
+      })
     }
-
-    /* ========================== */
-    /* service worker push相关部分 */
-    /* ========================== */
-    /**
-     * 注意这里修改了前一篇文章中service worker注册部分的代码
-     * 将service worker的注册封装为一个方法，方便使用
-     * @param {string} file service worker文件路径
-     * @return {Promise}
-     */
-    function registerServiceWorker(file) {
-        return navigator.serviceWorker.register(file);
-    }
-
-    /**
-     * 用户订阅相关的push信息
-     * 会生成对应的pushSubscription数据，用于标识用户与安全验证
-     * @param {ServiceWorker Registration} registration
-     * @param {string} publicKey 公钥
-     * @return {Promise}
-     */
-    function subscribeUserToPush(registration, publicKey) {
-        var subscribeOptions = {
-            userVisibleOnly: true,
-            applicationServerKey: window.urlBase64ToUint8Array(publicKey)
-        }; 
-        return registration.pushManager.subscribe(subscribeOptions).then(function (pushSubscription) {
-            console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
-            return pushSubscription;
-        });
-    }
-
-    /**
-     * 将浏览器生成的subscription信息提交到服务端
-     * 服务端保存该信息用于向特定的客户端用户推送
-     * @param {string} body 请求体
-     * @param {string} url 提交的api路径，默认为/subscription
-     * @return {Promise}
-     */
-    function sendSubscriptionToServer(body, url) {
-        url = url || '/subscription';
-        return new Promise(function (resolve, reject) {
-            var xhr = new XMLHttpRequest();
-            xhr.timeout = 60000;
-            xhr.onreadystatechange = function () {
-                var response = {};
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    try {
-                        response = JSON.parse(xhr.responseText);
-                    }
-                    catch (e) {
-                        response = xhr.responseText;
-                    }
-                    resolve(response);
-                }
-                else if (xhr.readyState === 4) {
-                    resolve();
-                }
-            };
-            xhr.onabort = reject;
-            xhr.onerror = reject;
-            xhr.ontimeout = reject;
-            xhr.open('POST', url, true);
-            xhr.setRequestHeader('Content-Type', 'application/json');
-            xhr.send(body);
-        });
-    }
-
-    if ('serviceWorker' in navigator && 'PushManager' in window) {
-        var publicKey = 'BOEQSjdhorIf8M0XFNlwohK3sTzO9iJwvbYU-fuXRF0tvRpPPMGO6d_gJC_pUQwBT7wD8rKutpNTFHOHN3VqJ0A';
-        // 注册service worker
-        registerServiceWorker('./sw.js').then(function (registration) {
-            console.log('Service Worker 注册成功');
-            // 开启该客户端的消息推送订阅功能
-            return subscribeUserToPush(registration, publicKey);
-        }).then(function (subscription) {
-            // 将生成的客户端订阅信息存储在自己的服务器上
-            return sendSubscriptionToServer(JSON.stringify(subscription));
-        }).then(function (res) {
-            console.log(res);
-        }).catch(function (err) {
-            console.log(err, 123456);
-        });
-    }
-    /* ========================== */
 })();
